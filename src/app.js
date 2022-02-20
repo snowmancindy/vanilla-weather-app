4f43660032212d9a4ead994b95fa3826efb8fe13
snowmancindy <yongyixu@gmail.com>
2022-02-20 16:24:38 +0800

    Added forecast javascript

--- src/app.js
+++ src/app.js
@@ -21,6 +21,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecast = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="forecast-date">${day}</div>
+      <img
+        src="https://openweathermap.org/img/wn/01d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="forecast-temperature">
+        <span class="forecast-max">18°</span>
+        <span class="forecast-min">12°</span>
+      </div>
+    </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecast.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperature = document.querySelector("#temperature");
   let city = document.querySelector("#city");
@@ -86,3 +114,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsius);
 
 search("New York");
+displayForecast();
